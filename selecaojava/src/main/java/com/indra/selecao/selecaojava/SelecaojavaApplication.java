0b70427eb7c633b8cd2651cc716e209ee5019303
Luan de Luna <luan.lunasantos@gmail.com>
2020-04-27 11:56:37 -0300

    Criando projeto

---
+++ selecaojava/src/main/java/com/indra/selecao/selecaojava/SelecaojavaApplication.java
@@ -0,0 +1,13 @@
+package com.indra.selecao.selecaojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaojavaApplication.class, args);
+	}
+
+}
